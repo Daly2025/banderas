5ac08df6eafbb29d2202ec3a00e5a68b177eacfb
Daly2025 <lydiafdez84@gmail.com>
2025-02-10 20:16:22 +0100

    cambios

--- js/banderas.js
+++ js/banderas.js
@@ -1,8 +1,9 @@
 function cargarBanderas(){
     fetch("https://restcountries.com/v3.1/all")
-    .then(response =>{
+    .then(response => {
         $("#cargando").hide();
-        return response.json()})
+        return response.json();
+    })
     .then(data => {
         let banderas = document.getElementById("banderas");
         for (let i = 0; i < data.length; i++) {
@@ -12,9 +13,15 @@ function cargarBanderas(){
             img.alt = data[i].name.common;
             banderas.appendChild(img);
         }
-    })
-    .catch(error => console.error(error))
-};
 
+        // Asignamos el evento de clic a las banderas una vez que están agregadas al DOM
+        $(".bandera").click(function(){
+            alert($(this).attr("alt"));
+        });
+    })
+    .catch(error => console.error(error));
+}
+
+// Llamamos a la función para cargar las banderas después de 2 segundos
 setTimeout(cargarBanderas, 2000);
 
